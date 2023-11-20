d62109e329789598689f5864f8bc8279d817f844
AmINecessary <147655777+AmINecessary@users.noreply.github.com>
2023-11-20 19:20:18 +0300

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ cevre1 = ["Geri dönüşüm ve hangi malzemelerin geri dönüştürülebileceği
            "Evde ampuller ve klimalar gibi enerji tasarruflu cihazlar kullanın.",
             "Ulaşım masraflarını azaltmak için yerel kaynaklardan yiyecek satın alın." ]
 
+cevre2 = ["Günde yaklaşık 27,000 ağaç kesiliyor.", "İnsanlar dünya'daki suyun sadece yüzde birini kullanabiliyor.", "Bütün karıncaların ağırlıkları bütün insanların ağırlıklarından daha fazla."]
+
+cevre3 = ["Dünya'daki suyun yüzde kaçı tatlı sudur? A) %3  B) %1,5  C) %2,5 D) %2", "Türkiye'nin yaklaşık olarak yüzde kaçı ormandır? A) %25  B) %30  C) %35  D) %40"]
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -35,4 +39,14 @@ async def cevre(ctx):
     bilgi = random.choice(cevre1)
     await ctx.send(f'İşte bir bilgi: {bilgi}')
 
-bot.run("Token")
+@bot.command()
+async def facts(ctx):
+    fact = random.choice(facts)
+    await ctx.send(f'İşte bir bilgi: {fact}')
+
+@bot.command()
+async def question(ctx):
+    soru = random.choice(cevre3)
+    await ctx.send(f'Soru: {soru}')
+
+bot.run("MTE2NjgwOTU1NjY0NzY5NDQ5Nw.GD3xYL.X-GTw7srfkIlIRQElW3ixCh51wTVswbiJxiffE")
